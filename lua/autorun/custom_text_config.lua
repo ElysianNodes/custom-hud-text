1cee108aa369c0a67d562aa7d8c31bb973aacb77
Jimputin <89787903+Jimputinfn@users.noreply.github.com>
2025-07-06 02:17:02 +0300

    changed default color since i forgot

--- lua/autorun/custom_text_config.lua
+++ lua/autorun/custom_text_config.lua
@@ -3,7 +3,7 @@
 
 CustomTextConfig = {
     text = "You can change this by editing custom_text_config.lua or console command: customtext_admin_gui !", -- The text to display
-    color = "red",                -- Preset color: red, blue, green, white, black
+    color = "white",                -- Preset color: red, blue, green, white, black
     position = "bottom_middle",   -- Position: bottom_middle, right_middle, left_middle, upper_middle, left_middle
     font = "Trebuchet24"           -- Font: DermaLarge, Default, Trebuchet24, TargetID, ChatFont, etc.
 } 
