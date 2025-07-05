a97c37048f5f40766f4c343721011050d6e845a2
Jimputin <89787903+Jimputinfn@users.noreply.github.com>
2025-07-06 02:16:14 +0300

    changed default font to the one i like :)

--- lua/autorun/custom_text_config.lua
+++ lua/autorun/custom_text_config.lua
@@ -5,5 +5,5 @@ CustomTextConfig = {
     text = "Hello from Jimputin!", -- The text to display
     color = "red",                -- Preset color: red, blue, green, white, black
     position = "bottom_middle",   -- Position: bottom_middle, right_middle, left_middle, upper_middle, left_middle
-    font = "DermaLarge"           -- Font: DermaLarge, Default, Trebuchet24, TargetID, ChatFont, etc.
+    font = "Trebuchet24"           -- Font: DermaLarge, Default, Trebuchet24, TargetID, ChatFont, etc.
 } 
