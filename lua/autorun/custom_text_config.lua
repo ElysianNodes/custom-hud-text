e320afa345f6ab074d50130a4ee6287bd75482fd
Jimputin <89787903+Jimputinfn@users.noreply.github.com>
2025-07-06 02:16:52 +0300

    chaanged default message

--- lua/autorun/custom_text_config.lua
+++ lua/autorun/custom_text_config.lua
@@ -2,7 +2,7 @@
 -- Addon By Jimputin @ ElysianNodes.uk
 
 CustomTextConfig = {
-    text = "Hello from Jimputin!", -- The text to display
+    text = "You can change this by editing custom_text_config.lua or console command: customtext_admin_gui !", -- The text to display
     color = "red",                -- Preset color: red, blue, green, white, black
     position = "bottom_middle",   -- Position: bottom_middle, right_middle, left_middle, upper_middle, left_middle
     font = "Trebuchet24"           -- Font: DermaLarge, Default, Trebuchet24, TargetID, ChatFont, etc.
